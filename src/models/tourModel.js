7d5a1082c8a7a23eecdde06c7669da2421f90992
Hamid Nazari <nazarihamid490@gmail.com>
2024-04-09 17:47:16 +0200

    initial commit

--- src/models/tourModel.js
+++ src/models/tourModel.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
-
+const slugify = require("slugify");
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true,'A tour must have a name'],
-        uniqe:true,
+        unique:true,
         trim:true,
+        maxlength:[40,'A tour name must have less or equal than 40 characters'],
+        minlength:[10,'A tour name must have more or equal than 10 characters'],
+        // validate:[validator.isAlpha,'Tour name must only contain characters']
     },
+    slug:String,
     duration:{
         type:Number,
         required:[true,'A tour must have a duration']
@@ -18,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty:{
         type:String,
-        required:[true,'A tour must have a difficulty']
+        required:[true,'A tour must have a difficulty'],
+        enum:{
+            values:['easy','medium','difficult'],
+            message:'Difficulty is either: easy, medium or difficult'
+        }
     },
     ratingsAverage:{
         type:Number,
-        default:4.5
+        default:4.5,
+        min:[1,'Rating must be above 1.0'],
+        max:[5,'Rating must be below 5.0'],
     },
     ratingsQuantity:{
         type:Number,
@@ -34,7 +45,13 @@ const tourSchema = new mongoose.Schema({
         required:[true,'A tour must have a price']
     },
     priceDiscount:{
-        type:Number
+        type:Number,
+        validate:{
+            validator:function(val){
+                return val < this.price;
+            },
+            message:'Discount price ({VALUE}) should be below regular price'
+        }
     },
     summary:{
         type:String,
@@ -56,6 +73,10 @@ const tourSchema = new mongoose.Schema({
         select:false
     },
     startDates:[Date],
+    secretTour:{
+        type:Boolean,
+        default:false
+    }
     
 },
 {
@@ -66,6 +87,28 @@ const tourSchema = new mongoose.Schema({
 
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7;
+});
+
+tourSchema.pre('save',function(next){
+
+    this.slug = slugify(this.name,{lower:true});
+
+    next();
+});
+
+tourSchema.pre(/^find/,function(next){
+
+    this.find({secretTour:{$ne:true}});
+
+    next();
+});
+
+
+tourSchema.pre('aggregate',function(next){
+
+    this.pipeline().unshift({$match:{secretTour:{$ne:true}}});
+
+    next();
 });
 
 
